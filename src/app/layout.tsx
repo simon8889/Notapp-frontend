e5a573c4dec05939c8103c3fd1d0d09259d45b06
simon <simondjv2016@gmail.com>
2024-12-14 03:10:17 -0500

    Page metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,20 +4,25 @@ import Header from "@/components/Header/Header";
 import "./layout.css";
 import useAuth from "@/hook/useAuth";
 
-const Layout = ({ children } : Readonly<{children: React.ReactNode}>) => {
+const Layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   const { token, checkAuth } = useAuth();
-  
+
   useEffect(() => {
     checkAuth()
   }, [token])
 
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="https://cdn.icon-icons.com/icons2/3266/ICO/512/git_repository_icon_207308.ico" />
+        <title>NotApp</title>
+        <meta name="description" content="Take, organize and update your notes!" />
+      </head>
       <body>
         <Header />
         {children}
       </body>
-    </html> 
+    </html>
 
   );
 }
